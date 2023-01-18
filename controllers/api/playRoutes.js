5b7082d24fc975b0b35ddf42ad718ce63d734684
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-01-18 12:49:55 -0800

    Adjusted all calls to render new page to pass user logged_in status

--- controllers/api/playRoutes.js
+++ controllers/api/playRoutes.js
@@ -4,7 +4,9 @@ const withAuth = require('../../utils/auth');
 
 router.get('/', withAuth, async (req, res) => {
     try {
-        res.render('play');
+        res.render('play', {
+            logged_in: req.session.logged_in
+        });
     } catch (err) {
         res.status(500).json(err);
     }
