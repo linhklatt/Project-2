f0a66f963a25f85d9ddab023c92351d41caf6c2c
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-01-19 21:56:50 -0800

    Added code for the buttons and stat allocation

--- public/js/end.js
+++ public/js/end.js
@@ -1,6 +1,28 @@
 const playerModel = $('#player');
 const player = getCharacter("playerData");
 
+const healthEl = $('#health');
+const attackEl = $('#attack');
+const counterEl = $('#counter');
+
+const healthDownBtn = $('#health-down');
+const healthUpBtn = $('#health-up');
+const attackDownBtn = $('#attack-down');
+const attackUpBtn = $('#attack-up');
+const counterDownBtn = $('#counter-down');
+const counterUpBtn = $('#counter-up');
+const homeBtn = $('#return-home');
+
+let health = player.health;
+let attack = player.attack;
+let counter = player.counter;
+
+const pointsEarned = 5;
+let pointsRemaining = pointsEarned;
+
+const pointsEl = $('#points-remaining');
+pointsEl.text(`You have ${pointsRemaining} points to spend`);
+
 function getCharacter(characterKey) {
     const characterData = localStorage.getItem(characterKey);
     const character = JSON.parse(characterData);
@@ -21,5 +43,103 @@ function walkRight(element, role) {
     }
 }
 
+function removePoint() {
+    pointsRemaining++;
+    if (pointsRemaining > 0) {
+        healthUpBtn.prop("disabled", false);
+        attackUpBtn.prop("disabled", false);
+        counterUpBtn.prop("disabled", false);
+    }
+    pointsEl.text(`You have ${pointsRemaining} points to spend`);
+};
+
+function spendPoint() {
+    pointsRemaining--;
+    if (pointsRemaining == 0) {
+        healthUpBtn.prop("disabled", true);
+        attackUpBtn.prop("disabled", true);
+        counterUpBtn.prop("disabled", true);
+    }
+    pointsEl.text(`You have ${pointsRemaining} points to spend`);
+};
+
+healthUpBtn.on('click', () => {
+    health++;
+    healthDownBtn.prop("disabled", false);
+    healthEl.text(health);
+
+    spendPoint();
+});
+
+healthDownBtn.on('click', () => {
+    health--;
+    if (health <= player.health) {
+        healthDownBtn.prop("disabled", true);
+    }
+    healthEl.text(health);
+    
+    removePoint();
+});
+
+attackUpBtn.on('click', () => {
+    attack++;
+    attackDownBtn.prop("disabled", false);
+    attackEl.text(attack);
+
+    spendPoint();
+});
+
+attackDownBtn.on('click', () => {
+    attack--;
+    if (attack <= player.attack) {
+        attackDownBtn.prop("disabled", true);
+    }
+    attackEl.text(attack);
+
+    removePoint();
+});
+
+counterUpBtn.on('click', () => {
+    counter++;
+    counterDownBtn.prop("disabled", false);
+    counterEl.text(counter);
+
+    spendPoint();
+});
+
+counterDownBtn.on('click', () => {
+    counter--;
+    if (counter <= player.counter) {
+        counterDownBtn.prop("disabled", true);
+    }
+    counterEl.text(counter);
+
+    removePoint();
+});
+
+const homeButtonHandler = async (event) => {
+    event.preventDefault();
+    
+    console.log('hello');
+    const response = await fetch(`/api/characters/${player.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ health, attack, counter }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    
+    if (response.ok) {
+        document.location.replace('/home');
+    } else {
+        alert(response.statusText);
+    }
+};
+
+homeBtn.on('click', homeButtonHandler);
+
 walkRight(playerModel, player.role);
 
+healthEl.text(health);
+attackEl.text(attack);
+counterEl.text(counter);
