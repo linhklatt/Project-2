4c277085d9d798c9f6ce6604f190c0687ae3af2b
Ryan Fernandez <ryanfernandez11@gmail.com>
2023-01-21 04:56:01 -0800

    Refactored pages with jQuery

--- public/js/create.js
+++ public/js/create.js
@@ -1,11 +1,90 @@
-const newCharacterButtonHandler = async(event) => {
+const roleList = $('.create-select');
+const roleDescription = $('.create-description');
+const nameForm = $('.name-form');
+const nameInput = $('#character-name');
+const backBtn = $('#create-back');
+const finishBtn = $('#create-finish');
+
+
+roleList.on('click', async(event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#character-name').value.trim();
-    const role = localStorage.getItem("role-create-js");
+    let selectedRoleTile = $(event.target).closest('div.create-tile')[0];
+
+    if (selectedRoleTile) {
+        let role = selectedRoleTile.getAttribute('role');
+
+        let lastSelectedRole = $('.selected-role');
+        let lastRole = localStorage.getItem("createRole");
+        $(lastSelectedRole).removeClass('selected-role');
+        $(lastSelectedRole).removeClass(`${lastRole}-walk-right`);
+
+        $(selectedRoleTile).addClass('selected-role');
+        $(selectedRoleTile).addClass(`${role}-walk-right`);
+
+        generateDescription(role);
+
+        localStorage.setItem("createRole", role);
+        nameForm.prop("hidden", false)
+    }
+});
+
+nameInput.on('input', () => {
+    if (nameInput.val()) {
+        finishBtn.prop("disabled", false);
+    } else {
+        finishBtn.prop("disabled", true);
+    }
+});
+
+function generateDescription (role) {
+    switch (role) {
+        case 'warrior':
+            roleDescription.text(
+                'warrior description'
+            );
+            break;
+        case 'archer':
+            roleDescription.text(
+                'archer description'
+            );
+            break;
+        case 'mage':
+            roleDescription.text(
+                'mage description'
+            );
+            break;
+        default:
+            roleDescription.text(
+                'Select your desired role.'
+            );
+            break;
+    }
+};
+
+backBtn.on('click', async(event) => {
+    event.preventDefault();
+
+    const response = await fetch('/home', {
+        method: 'GET'
+    });
+
+    if (response.ok) {
+        document.location.replace('/home');
+    } else {
+        alert(response.statusText);
+    }
+});
+
+finishBtn.on('click', async (event) => {
+    event.preventDefault();
+    
+    const name = await nameInput.val();
+    console.log(name);
+    const role = localStorage.getItem("createRole");
 
     if (name && role) {
-        const response = await fetch(`/api/characters/`, {
+        const response = await fetch('/api/characters/', {
             method: 'POST',
             body: JSON.stringify({ name, role }),
             headers: {
@@ -16,47 +95,18 @@ const newCharacterButtonHandler = async(event) => {
         if (response.ok) {
             document.location.replace('/home');
         } else {
-            // alert('Failed to create character');
-            var message = 'Failed to create character.';
-            $('#alertModal').find('.modal-body p').text(message);
-            $('#alertModal').modal('show');
+            alert('failed to create character');
         }
+    } else if (!role) {
+        let title = 'No Role Found';
+        let message = 'Please select a role for your character.';
+        $('#mainModal').find('.modal-title').text(title);
+        $('#mainModal').find('.modal-body p').text(message);
+        $('#mainModal').find('.btn-primary').hide();
+        $('#mainModal').modal('show');
     } else {
-        // alert('Please enter a name for your character.');
-        var message = 'Please enter a name for your character.';
-        $('#alertModal').find('.modal-body p').text(message);
-        $('#alertModal').modal('show');
+        alert('failed to create character');
     }
-};
+});
 
-const characterTileHandler = async(event) => {
-    if (event.target.hasAttribute('role')) {
-        const role = event.target.getAttribute('role');
-
-        localStorage.setItem("role-create-js", role);
-        
-        switch(role) {
-            case 'warrior':
-                document.querySelector('.character-description').textContent="warrior description"; 
-                break;
-            case 'archer':
-                document.querySelector('.character-description').textContent="archer description"; 
-                break;
-            case 'mage':
-                document.querySelector('.character-description').textContent="mage description"; 
-                break;
-        }
-
-        document.querySelector('.new-character-form').removeAttribute('hidden');
-    }
-};
-
-document
-    .querySelector('.new-character-form')
-    .addEventListener('submit', newCharacterButtonHandler);
-
-document
-    .querySelector('.character-tile')
-    .addEventListener('click', characterTileHandler);
-
-localStorage.setItem('role', '');
+localStorage.setItem('createRole', '');
--- public/js/userhome.js
+++ public/js/userhome.js
@@ -1,130 +1,96 @@
-const createButtonHandler = async (event) => {
-    event.preventDefault();
-    
+const createBtn = $('#create-character');
+const playBtn = $('#play');
+const deleteBtn = $('#delete-character');
+const modalOkay = $('.btn-primary');
+const charList = $('.user-home-char-list');
+
+createBtn.on('click', async () => {
     const response = await fetch('/api/characters/create', {
         method: 'GET'
     });
-    
+
     if (response.ok) {
         document.location.replace('api/characters/create');
     } else {
         alert(response.statusText);
     }
-};
+});
 
-const characterSelectHandler = async (event) => {
+charList.on('click', async (event) => {
     event.preventDefault();
 
-    if (event.target.hasAttribute('char-id')) {
-        const selectedChar = event.target;
-        const selectedCharId = selectedChar.getAttribute('char-id');
+    // Find the closest user-home-char-row element
+    let selectedChar = $(event.target).closest('div.user-home-char-row')[0];
 
+    // If a character was selected...
+    if (selectedChar) {
+        // Get the char-id
+        let selectedCharId = selectedChar.getAttribute('char-id');
+
+
+        // Store char-id into local storage
         localStorage.setItem("selectedCharId", selectedCharId);
-        
-        // TODO: Indicate highlight on selected character
-        const previouslySelected = localStorage.getItem("previouslySelected");
-        if (previouslySelected) {
-            document
-                .querySelector(`#player-character-${previouslySelected}`)
-                .setAttribute("class", "player-character");
-        }
-        const playerCharacter = document.querySelector(`#player-character-${selectedCharId}`);
-        playerCharacter.setAttribute("class", "player-character selected-character");
-        localStorage.setItem("previouslySelected", selectedCharId);
-
-        document.querySelector('#delete-btn').removeAttribute("hidden");
-        document.querySelector('#play-btn').removeAttribute("hidden");
-    }
-};
 
 
-const deleteButtonHandler = (event) => {
-    event.preventDefault();
+        // "Deselect" the last selected character
+        let lastSelected = $('.selected');
+        $(lastSelected).removeClass('selected');
 
-    const id = localStorage.getItem("selectedCharId");
 
-    if (id) {
-    //     if (confirm('Are you sure you want to delete this character?')) {
-    //         const response = await fetch(`/api/characters/${id}`, {
-    //             method: 'DELETE',
-    //         });
-    
-    //         if (response.ok) {
-    //             document.location.replace('/home');
-    //         } else {
-    //             alert('Failed to delete character.');
-    //         }
-    //     }
-    // } else {
-    //     alert('Please select a character to delete.');
-        var message = 'Are you sure you want to delete this character?';
-        $('#confirmModal').find('.c-modal-body p').text(message);
-        $('#confirmModal').modal('show');
-    } else {
-        var message = 'Please select a character to delete.'
-        $('#confirmModal').find('.c-modal-body p').text(message);
-        $('#confirm-delete').hide();
-        $('#confirmModal').modal('show');
-    // }
-}};
+        // Update the selected character on browser
+        $(selectedChar).addClass('selected');
 
-const confirmDeleteButton = document.getElementById('confirm-delete')
-confirmDeleteButton.addEventListener('click', async(event) => {
-    event.preventDefault();
-
-    const id = localStorage.getItem("selectedCharId");
-    const response = await fetch(`/api/characters/${id}`, {
-        method: 'DELETE',
-    });
-    if (response.ok) {
-        document.location.replace('/home');
-    } else {
-        var message = 'Failed to delete character. Please close and select a character.'
-        $('#confirmModal').find('.c-modal-body p').text(message);
-        $('#confirm-delete').hide();
-        $('#confirmModal').modal('show');
+        playBtn.prop("disabled", false);
+        deleteBtn.prop("disabled", false);
     }
 });
 
-const playButtonHandler = async (event) => {
+deleteBtn.on('click', async(event) => {
     event.preventDefault();
-
+   
     const id = localStorage.getItem("selectedCharId");
 
     if (id) {
-        // Get the player's selected character data
-        const playerData = await getCharacterData(id);
-        localStorage.setItem("playerData", JSON.stringify(playerData));
-        
-        // Get an array of all ids on the server, excluding the player's selected character id
-        const ids = await getCharacterIds(id);
-
-        // Passes the array of all ids and stores data for four unique opponents in local storage
-        await getOpponents(ids);
-        
-        // Fetch and render the /play page
-        const response = await fetch('api/play', {
-            method: 'GET'
-        });
-
-        if (response.ok) {
-            document.location.replace('api/play');
-        } else {
-            alert(response.statusText)
-        }
+        let title = 'Delete Character';
+        let message = 'You are about to permanently delete a character. Press Okay to proceed.';
+        let btnText = 'Okay';
+        $('#mainModal').find('.modal-title').text(title);
+        $('#mainModal').find('.modal-body p').text(message);
+        $('#mainModal').find('.btn-primary').text(btnText);
+        $('#mainModal').find('.btn-primary').show();
+        $('#mainModal').modal('show');
     } else {
-        // alert('Please select a character first.');
-        var message = 'Please select a character.'
-        $('#confirmModal').find('.c-modal-body p').text(message);
-        $('#confirm-delete').hide();
-        $('#confirmModal').modal('show');
+        let title = 'No Character Selected';
+        let message = 'Please select a character to delete.';
+        $('#mainModal').find('.modal-title').text(title);
+        $('#mainModal').find('.modal-body p').text(message);
+        $('#mainModal').find('.btn-primary').hide();
+        $('#mainModal').modal('show');
     }
-};
+});
+
+modalOkay.on('click', async(event) => {
+    $('#mainModal').modal('hide');
+    
+    const id = localStorage.getItem("selectedCharId");
+    if (id) {
+        const response = await fetch(`/api/characters/${id}`, {
+            method: 'DELETE'
+        });
+    
+        if (response.ok) {
+            document.location.replace('/home');
+        } else {
+            alert(response.statusText);
+        }
+    }
+});
 
 async function getCharacterData(id) {
     const response = await fetch(`/api/characters/${id}`, {
         method: 'GET'
     });
+
 
     if (response.ok) {
         const characterData = await response.json();
@@ -134,10 +100,12 @@ async function getCharacterData(id) {
     }
 };
 
+
 async function getCharacterIds(id) {
     const response = await fetch(`/api/characters/get-ids/${id}`, {
         method: 'GET'
     });
+
 
     if (response.ok) {
         const idData = await response.json();
@@ -148,44 +116,77 @@ async function getCharacterIds(id) {
     }
 };
 
+
 async function getOpponents(ids) {
     // Loop four times to get four opponents for the player
     for (let i = 0; i < 4; i++) {
         // Get a random index from the ids array
         const randomIndex = Math.floor((Math.random()*ids.length));
 
+
         // Store the opponent id of the random index
         const opponentId = ids[randomIndex];
+
 
         // Remove the selected opponent id from the ids array
         ids.splice(randomIndex, 1);        
 
+
         // Fetch the opponent's character data
         const opponentData = await getCharacterData(opponentId);
+
 
         // Store the opponent's character data in local storage
         localStorage.setItem(`opponentData${i+1}`, JSON.stringify(opponentData));
     }
 };
 
-document
-    .querySelector('.create-character')
-    .addEventListener('click', createButtonHandler);
 
-document
-    .querySelector('.player-character-list')
-    .addEventListener('click', characterSelectHandler);
+playBtn.on('click', async(event) => {
+    event.preventDefault();
 
-document
-    .querySelector('.delete-character')
-    .addEventListener('click', deleteButtonHandler);
 
-document
-    .querySelector('.play-game')
-    .addEventListener('click', playButtonHandler);
+    const id = localStorage.getItem("selectedCharId");
+
+
+    if (id) {
+        // Get the selected character's data
+        const playerData = await getCharacterData(id);
+        localStorage.setItem("playerData", JSON.stringify(playerData));
+
+
+        // Get an array of all ids on the server, excluding the player's selected character id
+        const ids = await getCharacterIds(id);
+
+
+        // Passes the array of all ids and stores data for four unique opponents in local storage
+        await getOpponents(ids);
+
+
+        // Fetch and render the /play page
+        const response = await fetch('api/play', {
+            method: 'GET'
+        });
+
+
+        if (response.ok) {
+            document.location.replace('api/play');
+        } else {
+            alert(response.statusText)
+        }
+    } else {
+        let title = 'No Character Selected'
+        let message = 'Please select a character to play.';
+       
+        $('#mainModal').find('.modal-title').text(title);
+        $('#mainModal').find('.modal-body p').text(message);
+        $('#mainModal').find('.btn-primary').hide();
+        $('#mainModal').modal('show');
+    }
+});
+
 
 localStorage.setItem("selectedCharId", '');
-localStorage.setItem("previouslySelected", '');
 localStorage.setItem("playerData", '');
 localStorage.setItem("opponentData1", '');
 localStorage.setItem("opponentData2", '');
